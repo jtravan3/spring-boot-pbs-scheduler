b7dd65c0b1fa4b48fc4805b99c9ce0b6b82e9f73
John Ravan <john@jtravan.com>
2018-05-17 10:52:49 -0400

    One file was left behind

---
+++ src/main/java/com/jtravan/pbs/model/ResourceCategoryDataStructure_READ.java
@@ -0,0 +1,6 @@
+package com.jtravan.pbs.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResourceCategoryDataStructure_READ extends ResourceCategoryDataStructure {}
